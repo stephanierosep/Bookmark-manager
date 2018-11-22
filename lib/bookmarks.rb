265ae8da159d27583136d833c55bf5c1923c3dbb
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-22 16:47:54 +0000

    refactored test environment

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,5 +1,5 @@
 require 'pg'
-
+require 'database_connection'
 
 class Bookmark
 
@@ -12,12 +12,7 @@ class Bookmark
     end
 
     def self.list
-      if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-      else
-      connection = PG.connect(dbname: 'bookmark_manager')
-      end
-      result = connection.exec("SELECT * FROM bookmarks")
+      result = DatabaseConnection.query("SELECT * FROM bookmarks")
       result.map do |bookmark|
         Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
       end
@@ -26,12 +21,7 @@ class Bookmark
     def self.create(url:, title:)
 
       return false unless is_url?(url)
-      if ENV['ENVIRONMENT'] == 'test'
-        connection = PG.connect(dbname: 'bookmark_manager_test')
-      else
-        connection = PG.connect(dbname: 'bookmark_manager')
-      end
-      result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+      result = DatabaseConnection.query("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
       Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
     end
 
@@ -40,31 +30,16 @@ class Bookmark
     end
 
     def self.delete(id)
-      if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-      else
-      connection = PG.connect(dbname: 'bookmark_manager')
-      end
-      connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+      DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id}")
     end
 
     def self.update(id:, title:, url:)
-      if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-      else
-      connection = PG.connect(dbname: 'bookmark_manager')
-      end
-      result = connection.exec("UPDATE bookmarks SET title = '#{title}', url = '#{url}' WHERE id = #{id} RETURNING id, url, title")
+      result = DatabaseConnection.query("UPDATE bookmarks SET title = '#{title}', url = '#{url}' WHERE id = #{id} RETURNING id, url, title")
       Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
     end
 
     def self.find(id:)
-      if ENV['ENVIRONMENT'] == 'test'
-        connection = PG.connect(dbname: 'bookmark_manager_test')
-      else
-        connection = PG.connect(dbname: 'bookmark_manager')
-      end
-      result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+      result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id};")
       Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
     end
 end
--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -6,5 +6,12 @@ describe DatabaseConnection do
       expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
       DatabaseConnection.setup('bookmark_manager_test')
     end
+
+  describe '.query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(connection).to receive(:exec).with("SELECT * FROM bookmarks;")
+      DatabaseConnection.query("SELECT * FROM bookmarks;")
+    end
   end
 end
